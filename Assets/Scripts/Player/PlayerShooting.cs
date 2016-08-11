c1625bc3929c7bc3c9ba41d56dc751a10086c371
denis.gerasimuk <denis.gerasimuk@mail.ru>
2016-08-11 21:59:24 +0400

    7 урок 23.50

---
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShooting : MonoBehaviour
+{
+    public int damagePerShot = 20;
+    public float timeBetweenBullets = 0.15f;
+    public float range = 100f;
+
+    float timer;
+    Ray shootRay;
+    RaycastHit shootHit;
+    int shootableMask;
+    ParticleSystem gunParticles;
+    LineRenderer gunLine;
+    AudioSource gunAudio;
+    Light gunLight;
+    float effectsDisplayTime = 0.2f;
+
+    void Awake()
+    {
+        shootableMask = LayerMask.GetMask("Shootable");
+        gunParticles = GetComponent<ParticleSystem>();
+        gunLine = GetComponent<LineRenderer>();
+        gunAudio = GetComponent<AudioSource>();
+        gunLight = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && timer >= timeBetweenBullets)
+        {
+            Shoot();
+        }
+
+        if (timer >= timeBetweenBullets * effectsDisplayTime)
+        {
+            DisableEffects();
+        }
+    }
+
+
+}
